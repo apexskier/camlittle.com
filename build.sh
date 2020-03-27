76495ae4a0ace3b48261812b112cafaa7bdc0589
Cameron Little <cameron@camlittle.com>
2020-03-27 08:07:46 +0100

    inline social icons

--- build.sh
+++ build.sh
@@ -4,4 +4,4 @@ set -e
 
 hugo gen chromastyles --style=manni > assets/css/highlight_light.css
 hugo gen chromastyles --style=native > assets/css/highlight_dark.css
-hugo
+hugo --minify
---
+++ download_social.sh
@@ -0,0 +1,11 @@
+rm asset/social_icons/*
+
+curl -L -o ./assets/social_icons/fa-codepen.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/codepen.svg
+curl -L -o ./assets/social_icons/fa-github.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/github.svg
+curl -L -o ./assets/social_icons/fa-instagram.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/instagram.svg
+curl -L -o ./assets/social_icons/fa-linkedin.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/linkedin.svg
+curl -L -o ./assets/social_icons/fa-map-marked.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/solid/map-marked.svg
+curl -L -o ./assets/social_icons/fa-strava.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/strava.svg
+curl -L -o ./assets/social_icons/fa-twitter.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/twitter.svg
+curl -L -o ./assets/social_icons/fa-vimeo-v.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/vimeo-v.svg
+curl -L -o ./assets/social_icons/fa-youtube.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/youtube.svg
