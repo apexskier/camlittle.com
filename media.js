befbb8fa9f63f104c4edae05615d7bfb18d3dd16
Cameron Little <cameron@camlittle.com>
2021-06-03 20:12:49 +0200

    Serialize media upload to try to avoid 503s

--- media.js
+++ media.js
@@ -57,81 +57,72 @@ const dryRun = process.argv.includes("--dry-run");
     });
   }
 
-  const work = [];
+  for (const fileData of imageMeta) {
+    for (const mediaData of fileData.media) {
+      const parsedPermalink = path.parse(mediaData.rel_permalink);
+      if (!mediaData.image_pipeline) {
+        // not an image
+        return;
+      }
 
-  await Promise.all(
-    imageMeta
-      .filter((fileData) => fileData.media)
-      .map((fileData) => fileData.media)
-      .flat()
-      .map((mediaData) =>
-        (async () => {
-          const parsedPermalink = path.parse(mediaData.rel_permalink);
-          if (!mediaData.image_pipeline) {
-            // not an image
-            return;
-          }
+      const keyPrefix = `site-media${parsedPermalink.dir}/${parsedPermalink.name}_${mediaData.hash}`;
+      const inputBuffer = await readFile(
+        path.join(process.cwd(), "content", mediaData.filepath)
+      );
+      console.log(
+        `original ${mediaData.filepath} ${prettyBytes(
+          Buffer.byteLength(inputBuffer)
+        )}`
+      );
+      const image = sharp(inputBuffer);
 
-          const keyPrefix = `site-media${parsedPermalink.dir}/${parsedPermalink.name}_${mediaData.hash}`;
-          const inputBuffer = await readFile(
-            path.join(process.cwd(), "content", mediaData.filepath)
-          );
-          console.log(
-            `original ${mediaData.filepath} ${prettyBytes(
-              Buffer.byteLength(inputBuffer)
-            )}`
-          );
-          const image = sharp(inputBuffer);
-
-          for (const format of ["jpg", "webp"]) {
-            for (const scale of [1, 2]) {
-              for (const res of imageSources.widths) {
-                // NOTE: "@" isn't supported in s3 key names
-                const key = `${keyPrefix}_${res}x0at${scale}x.${format}`;
-                const keyExists = await objectExists(key);
-                if (keyExists) {
-                  console.log(`already exists ${key}`);
-                } else {
-                  work.push(
-                    (async () => {
-                      let img = image.clone().resize(res).toFormat(format);
-                      if (format == "jpg") {
-                        img = img.jpeg({ progressive: true, quality: 90 });
-                      }
-                      const body = await img.toBuffer();
-                      if (dryRun) {
-                        console.log(`would upload ${key}`);
-                      } else {
-                        const data = await new Promise((resolve, reject) => {
-                          s3.upload(
-                            {
-                              Key: key,
-                              Body: body,
-                              Bucket: "camlittle-content",
-                              ACL: "public-read",
-                            },
-                            (err, data) => {
-                              if (err) {
-                                reject(err);
-                              } else {
-                                resolve(data);
-                              }
-                            }
-                          );
-                        });
-                        console.log(`uploaded ${data.Key}`);
-                      }
-                    })()
-                  );
-                }
-              }
+      const work = [];
+      for (const format of ["jpg", "webp"]) {
+        for (const scale of [1, 2]) {
+          for (const res of imageSources.widths) {
+            // NOTE: "@" isn't supported in s3 key names
+            const key = `${keyPrefix}_${res}x0at${scale}x.${format}`;
+            if (await objectExists(key)) {
+              console.log(`already exists ${key}`);
+            } else {
+              work.push(
+                (async () => {
+                  let img = image.clone().resize(res).toFormat(format);
+                  if (format == "jpg") {
+                    img = img.jpeg({ progressive: true, quality: 90 });
+                  }
+                  const body = await img.toBuffer();
+                  if (dryRun) {
+                    console.log(`would upload ${key}`);
+                  } else {
+                    const data = await new Promise((resolve, reject) => {
+                      s3.upload(
+                        {
+                          Key: key,
+                          Body: body,
+                          Bucket: "camlittle-content",
+                          ACL: "public-read",
+                        },
+                        (err, data) => {
+                          if (err) {
+                            reject(err);
+                          } else {
+                            resolve(data);
+                          }
+                        }
+                      );
+                    });
+                    console.log(`uploaded ${data.Key}`);
+                  }
+                })()
+              );
             }
           }
-        })()
-      )
-  );
-
-  await Promise.all(work);
+        }
+      }
+      await Promise.all(work);
+    }
+  }
 })().catch((err) => {
   console.error(err);
   process.exit(2);
