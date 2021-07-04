708a539e88d52b224b24fc4c38390ccb40292f5a
Cameron Little <cameron@camlittle.com>
2021-07-04 21:46:18 +0200

    Add custom gaia gps icon on social

--- download_social.sh
+++ download_social.sh
@@ -6,7 +6,6 @@ curl -L -o ./assets/social_icons/fa-codepen.svg https://github.com/FortAwesome/F
 curl -L -o ./assets/social_icons/fa-github.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/github.svg
 curl -L -o ./assets/social_icons/fa-instagram.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/instagram.svg
 curl -L -o ./assets/social_icons/fa-linkedin.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/linkedin.svg
-curl -L -o ./assets/social_icons/fa-map-marked.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/solid/map-marked.svg
 curl -L -o ./assets/social_icons/fa-strava.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/strava.svg
 curl -L -o ./assets/social_icons/fa-twitter.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/twitter.svg
 curl -L -o ./assets/social_icons/fa-vimeo-v.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/vimeo-v.svg
