e36933a7a986844144abd6d8be10fdffbbc00d06
Cameron Little <cameron@camlittle.com>
2024-03-05 11:31:37 -0700

    Add slopes icon

--- download_social.sh
+++ download_social.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-rm asset/social_icons/*
+rm assets/social_icons/fa-*
 
-curl -L -o ./assets/social_icons/fa-codepen.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/codepen.svg
-curl -L -o ./assets/social_icons/fa-github.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/github.svg
-curl -L -o ./assets/social_icons/fa-instagram.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/instagram.svg
-curl -L -o ./assets/social_icons/fa-linkedin.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/linkedin.svg
-curl -L -o ./assets/social_icons/fa-strava.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/strava.svg
-curl -L -o ./assets/social_icons/fa-twitter.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/twitter.svg
-curl -L -o ./assets/social_icons/fa-vimeo-v.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/vimeo-v.svg
-curl -L -o ./assets/social_icons/fa-youtube.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/youtube.svg
-curl -L -o ./assets/social_icons/fa-stack-overflow.svg https://github.com/FortAwesome/Font-Awesome/raw/master/svgs/brands/stack-overflow.svg
+curl -L -o ./assets/social_icons/fa-codepen.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/codepen.svg
+curl -L -o ./assets/social_icons/fa-github.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/github.svg
+curl -L -o ./assets/social_icons/fa-instagram.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/instagram.svg
+curl -L -o ./assets/social_icons/fa-linkedin.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/linkedin.svg
+curl -L -o ./assets/social_icons/fa-strava.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/strava.svg
+curl -L -o ./assets/social_icons/fa-twitter.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/twitter.svg
+curl -L -o ./assets/social_icons/fa-vimeo-v.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/vimeo-v.svg
+curl -L -o ./assets/social_icons/fa-youtube.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/youtube.svg
+curl -L -o ./assets/social_icons/fa-stack-overflow.svg https://github.com/FortAwesome/Font-Awesome/raw/6.x/svgs/brands/stack-overflow.svg
